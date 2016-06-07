4a0b7d73f2ba8db8c94ec35ec25a0e5b425fd4af
Thomas-Ko <tomkolodzi@gmail.com>
2016-06-07 15:24:18 -0400

    Add an unbind event to color handler

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 model = {
-	turnedOn : false,
-	turn : {
-		computer: true,
-		player: false,
-	},
+	// turnedOn : false,
+	// turn : {
+	// 	computer: true,
+	// 	player: false,
+	// },
 	colors: ["green", "red", "yellow", "blue"],
 
-	strict: false,
+	// strict: false,
 	
 	computer: {
 		colorSequence:[],
@@ -53,7 +53,7 @@ controller = {
 			if (model.player.turnStatus===false){
 				model.player.turnStatus=true;
 				model.computer.turnStatus=false;
-			} else {
+			} else if(model.player.turnStatus===true){
 				model.player.turnStatus=false;
 				model.computer.turnStatus=true;
 			}
@@ -75,17 +75,28 @@ controller = {
 	},
 
 	checkColor: function(color){
-		if(model.turn.player){
+		console.log("controller.checkColor start");
+		// if(model.player.turnStatus){
 			model.player.colorSequence.push(color);
 			if (model.player.colorSequence[model.player.colorCount]===model.computer.colorSequence[model.player.colorCount]){
 				console.log("heck yeah");
 			} else {
 			console.log("nooo");
+			model.player.colorCount=-1;
+			model.player.colorSequence=[];
+			// controller.turn.switch();
+			model.player.turnStatus=false;
+			// view.colorHandler();
+
+			model.computer.colorCount=0;
+
+			controller.colorSequence.addNewColorTo();
+			view.displayColorSequence();
 			}
-		}
+		// }
 		
 
-		console.log("check color end");
+		console.log("controller.checkColor end");
 	},
 
 	turnGameOnOrOff: function(){
@@ -123,24 +134,31 @@ view = {
 
 	colorHandler: function(){
 		//if player's turn is true
-		if(controller.turn.getStatus("player")){
+		
 			
 			$(".color-outer").on("mousedown", ".color", function(){
-				var color = $(this).attr('id');
-				var audio = document.getElementById(color+"Audio");
-				$(this).addClass(color+"-bg-active");
-				audio.play();
-				model.player.colorCount++;
-				controller.checkColor(color);
+				var status = controller.turn.getStatus("player");
+				console.log("the status is "+model.player.turnStatus);
+				console.log("you pressed "+$(this).attr('id'));
+				console.log("---");
 
-		
+				if(controller.turn.getStatus("player")){
+					var color = $(this).attr('id');
+					var audio = document.getElementById(color+"Audio");
+					$(this).addClass(color+"-bg-active");
+					audio.play();
+					model.player.colorCount++;
+					controller.checkColor(color);
+				}
+				$(this).unbind();
 			});
 
 			//when the mouse is released, the color turns off,
 			$(window).on("mouseup",function(){
 				$(".color").removeClass("green-bg-active red-bg-active yellow-bg-active blue-bg-active");
 			});
-		}
+
+		// }
 	},
 
 	displayColorSequence: function(){
@@ -160,7 +178,7 @@ view = {
 	      		$("#"+color).addClass(color+"-bg-active"); 
 	      	} else if (colorCount===colorSequence.length){
 	      		controller.turn.switch();
-	      		view.colorHandler();
+	      		// view.colorHandler();
 	      	}     
 	      	
 	      	controller.colorCount.addTo();
