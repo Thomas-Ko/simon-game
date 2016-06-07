cb8a2f74c58c1b0aec0020c33a038f461491570f
Thomas-Ko <tomkolodzi@gmail.com>
2016-06-07 11:21:31 -0400

    Refactor code

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ model = {
 	strict: false,
 	count: null,
 	colorSequence: [],
-	i: 0,
+	colorCount: 0,
 
 };
 
@@ -17,12 +17,37 @@ controller = {
 
 	},
 
-	chooseColor: function(){
-		var num = Math.floor((Math.random() * 4));
-		var color = model.colors[num];
-		// console.log(color);
-		model.colorSequence.push(color);
+	colorCount: {
+		addTo: function(){
+			model.colorCount++;
+		},
+		get: function(){
+			return model.colorCount;
+		},
+		resetToZero: function(){
+			model.colorCount=0;
+		}
 	},
+
+	colorSequence: {
+		get: function(){
+			return model.colorSequence;
+		},
+		addto: function(color){
+
+		},
+		addNewColorTo: function(){
+			var num = Math.floor((Math.random() * 4));
+			var color = model.colors[num];
+			model.colorSequence.push(color);
+		}
+	},
+
+	// chooseColor: function(){
+	// 	var num = Math.floor((Math.random() * 4));
+	// 	var color = model.colors[num];
+	// 	model.colorSequence.push(color);
+	// },
 
 
 	displayColor: function(color){
@@ -38,8 +63,8 @@ view = {
 
 	startButtonHandler : function(){
 		$("#start").on("click", function(){
-			model.i=0;
-			controller.chooseColor();
+			controller.colorCount.resetToZero();
+			controller.colorSequence.addNewColorTo();
 			view.displayColorSequence();
 		});
 	},
@@ -54,17 +79,20 @@ view = {
 
 
 		setTimeout(function () {    
-	      	// $(".color").removeClass("green-bg-active red-bg-active yellow-bg-active blue-bg-active");
-	      	if(model.i<model.colorSequence.length){
-	      		var color = model.colorSequence[model.i];
+	      	var colorCount = controller.colorCount.get();
+	      	var colorSequence = controller.colorSequence.get();
+	      	if(colorCount<colorSequence.length){
+	      		var color = colorSequence[colorCount];
 	      		var audio = document.getElementById(color+"Audio");
 				audio.play();
 	      		$("#"+color).addClass(color+"-bg-active"); 
 
 	      	}       
 	      	
-	      	model.i++;                     
-	      	if (model.i <= model.colorSequence.length) {          
+	      	controller.colorCount.addTo();
+	      	//colorCount must be established again because of the addTo above
+	      	colorCount = controller.colorCount.get();                     
+	      	if (colorCount <= colorSequence.length) {          
 	         	view.displayColorSequence();           
 	      	}                       
 	   }, 1500);
