71d62a2c6bd1a5373350895c5055b83888ad95dd
Thomas-Ko <tomkolodzi@gmail.com>
2016-06-09 16:05:10 -0400

    Clean up code, add comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,12 @@
+/*====================
+	MODEL
+====================*/
 model = {
-	// turnedOn : false,
-	// turn : {
-	// 	computer: true,
-	// 	player: false,
-	// },
+	//DO NOT CHANGE ANYTHING except for winningNumber if you wish.
 	colors: ["green", "red", "yellow", "blue"],
 	count: 0,
 	strict: false,
 	winningNumber:20,
-
-	// strict: false,
 	
 	computer: {
 		colorSequence:[],
@@ -17,56 +14,26 @@ model = {
 		turnStatus: true,
 	},
 
-	// colorSequence: [],
-	// colorCount: 0,
-
 	player: {
 		colorCount:-1,
 		colorSequence:[],
 		turnStatus: false,
 	},
 
-	// playerMovesCount: -1,
-	// playerColorSequence:[],
-
 };
 
+
+/*====================
+	CONTROLLER
+====================*/
 controller = {
 	init: function(){
-
+		view.init();
 	},
 
 	count : {
 		get: function(){
 			return model.count;
-		}
-	},
-
-	colorCount: {
-		addTo: function(){
-			model.computer.colorCount++;
-		},
-		get: function(){
-			return model.computer.colorCount;
-		},
-		reset: function(){
-			model.computer.colorCount=0;
-		}
-	},
-
-	turn: {
-		getStatus : function(playerOrComp){
-			return model[playerOrComp].turnStatus;
-		},
-		set: function(computerOrPlayer){
-			if(computerOrPlayer==="computer"){
-				model.player.turnStatus=false;
-				model.player.computer=true;
-			} else if(computerOrPlayer==="player"){
-				model.player.turnStatus=true;
-				model.player.computer=false;
-			}
-			
 		}
 	},
 
@@ -80,83 +47,75 @@ controller = {
 			} else if(onOrOff==="off"){
 				model.strict=false;
 			} 
-			// if(model.strict===false){
-			// 	model.strict=true;
-			// } else {
-			// 	model.strict=false;
-			// }
-			// }
 		}
 	},
 
-	colorSequence: {
-		get: function(){
-			return model.computer.colorSequence;
+	//all of these interact with the 
+	computer: {
+		colorCount: {
+			addTo: function(){
+				model.computer.colorCount++;
+			},
+			get: function(){
+				return model.computer.colorCount;
+			},
+			reset: function(){
+				model.computer.colorCount=0;
+			}
 		},
-		addto: function(color){
 
+		colorSequence: {
+			get: function(){
+				return model.computer.colorSequence;
+			},
+			addTo: function(){
+				var num = Math.floor((Math.random() * 4));
+				var color = model.colors[num];
+				model.computer.colorSequence.push(color);
+			}
 		},
-		addNewColorTo: function(){
-			var num = Math.floor((Math.random() * 4));
-			var color = model.colors[num];
-			model.computer.colorSequence.push(color);
+	},
+
+	turn: {
+		getStatus : function(playerOrComp){
+			return model[playerOrComp].turnStatus;
+		},
+		set: function(computerOrPlayer){
+			if(computerOrPlayer==="computer"){
+				model.player.turnStatus=false;
+				model.player.computer=true;
+			} else if(computerOrPlayer==="player"){
+				model.player.turnStatus=true;
+				model.player.computer=false;
+			}	
 		}
 	},
 
 	checkColor: function(color){
-		console.log("*****controller.checkColor start");
-		// if(model.player.turnStatus){
-			model.player.colorSequence.push(color);
-			console.log("player count is "+model.player.colorCount);
-			console.log("player's color sequence is: " + model.player.colorSequence);
-			console.log("computer's color sequence is: " + model.computer.colorSequence);
-			console.log("model.player.colorSequence[model.player.colorCount] is: "+model.player.colorSequence[model.player.colorCount]);
-			console.log("model.computer.colorSequence[model.player.colorCount] is: "+ model.computer.colorSequence[model.player.colorCount]);
-
-			if (model.player.colorSequence[model.player.colorCount]===model.computer.colorSequence[model.player.colorCount]){
-				console.log("heck yeah");
-				view.colorSound(color);
-				if(model.player.colorCount===model.computer.colorCount-2 && model.player.colorCount===model.winningNumber-1){
-					console.log("last turn");
-					console.log(model.computer.colorCount-2);
-					view.winnerSoundAndDisplay(color);
-					setTimeout(function(){
-						controller.resetGame();
-						controller.computerTurn("new");
-					},1300);
-					
-				} else if(model.player.colorCount===model.computer.colorCount-2){
+		model.player.colorSequence.push(color);
+		
+		if (model.player.colorSequence[model.player.colorCount]===model.computer.colorSequence[model.player.colorCount]){
+			view.colorSound(color);
+			if(model.player.colorCount===model.computer.colorCount-2 && model.player.colorCount===model.winningNumber-1){
+				view.winnerSoundAndDisplay(color);
+				setTimeout(function(){
+					controller.resetGame();
 					controller.computerTurn("new");
-				}
-			} else {
-				view.errorSoundAndDisplay();
+				},1300);	
+			} else if(model.player.colorCount===model.computer.colorCount-2){
+				controller.computerTurn("new");
+			}
+		} else {
+			view.errorSoundAndDisplay();
 			if(!model.strict){
 				controller.computerTurn("repeat");
 			} else if(model.strict){
-			setTimeout(function () {
-        		controller.resetGame();
-				controller.computerTurn("new");
-    		}, 500);
-
-
-			}
-			// controller.computerTurn("repeat");
-			// model.player.colorCount=-1;
-			// model.computer.colorCount=0;
-			// model.player.colorSequence=[];
-			// controller.turn.switch();
-
-			// controller.newComputerTurn();
-
-			// controller.colorSequence.addNewColorTo();
-			// view.displayColorSequence();
-			}
-		// }
-
-		
-
-		console.log("*****controller.checkColor end");
-		// console.log("============================");
+				setTimeout(function () {
+        			controller.resetGame();
+					controller.computerTurn("new");
+    			}, 500);
+			}	
+		}	
 	},
 
 	computerTurn: function(repeatOrNew){
@@ -167,23 +126,11 @@ controller = {
 		controller.turn.set("computer");
 		view.displayTurn("computer");
 		if(repeatOrNew==="new"){
-			controller.colorSequence.addNewColorTo();
+			controller.computer.colorSequence.addTo();
 			model.count++;
 			view.displayCount();
 		}
 		view.displayColorSequence();
-	},
-
-	redoComputerSequence: function(){
-		model.player.colorCount=-1;
-		model.computer.colorCount=0;
-	},
-
-	turnGameOnOrOff: function(){
-		if (model.turnedOn===false){
-			model.turnedOn=true;
-
-		}
 	},
 
 	resetGame: function(){
@@ -196,22 +143,22 @@ controller = {
 		view.displayTurn("computer");
 	},
 
-
-
-	displayColor: function(color){
-		setTimeout(function () {
-        		$("#"+color).addClass(color+"-bg-active");
-    		}, 1000);
-
-		$("#"+color).removeClass(color+"-bg-active");
-	}
 }; //end controller
 
+
+/*====================
+	VIEW
+====================*/
 view = {
+
+	init: function(){
+		view.startButtonHandler();
+		view.colorHandler();
+		view.strictButtonHandler();
+	},
 
 	startButtonHandler : function(){
 		$("#start").on("click", function(){
-			// model.computer.colorCount=0;
 			controller.resetGame();
 			controller.computerTurn("new");
 		});
@@ -232,37 +179,23 @@ view = {
 		});
 	},
 
-	colorHandler: function(){
-			
-			$(".color-outer").on("mousedown", ".color", function(){
-				console.log("============================");
-				console.log("*****colorHandler mousedown start");
-				var status = controller.turn.getStatus("player");
-				console.log("the status is "+model.player.turnStatus);
-				console.log("you pressed "+$(this).attr('id'));
+	colorHandler: function(){	
+		$(".color-outer").on("mousedown", ".color", function(){
+			var status = controller.turn.getStatus("player");
 
-				if(controller.turn.getStatus("player")){
+			if(controller.turn.getStatus("player")){
+				var color = $(this).attr('id');
+				$(this).addClass(color+"-bg-active");
+				model.player.colorCount++;
+				controller.checkColor(color);
+			}
+			$(this).unbind();
+		});
 
-					var color = $(this).attr('id');
-					
-					$(this).addClass(color+"-bg-active");
-					// view.colorSound(color);
-					// var audio = document.getElementById(color+"Audio");
-					// audio.play();
-					model.player.colorCount++;
-					controller.checkColor(color);
-				}
-				$(this).unbind();
-				console.log("*****colorHandler mousedown end");
-				
-			});
-
-			//when the mouse is released, the color turns off,
-			$(window).on("mouseup",function(){
-				$(".color").removeClass("green-bg-active red-bg-active yellow-bg-active blue-bg-active");
-			});
-
-		// }
+		//when the mouse is released, the color turns off,
+		$(window).on("mouseup",function(){
+			$(".color").removeClass("green-bg-active red-bg-active yellow-bg-active blue-bg-active");
+		});
 	},
 
 	colorCursor: {
@@ -289,6 +222,7 @@ view = {
 		}, 550);
 	},
 
+	//this is a really messy way of making a flashing animation, but it get's the job done, plus it only runs when the user wins
 	winnerSoundAndDisplay : function(color){
 		var colorArray = ["green","red","yellow","blue"];
 		var index = colorArray.indexOf(color);
@@ -299,8 +233,7 @@ view = {
 		var audio4 = document.getElementById(colorArray[2]+"Audio");
 
 		console.log(audio1+","+audio2+","+audio3+","+audio4);
-		var audio = document.getElementById("winnerAudio");
-		// audio.play();	
+		var audio = document.getElementById("winnerAudio");	
 	   	setTimeout(function () {    
 	      	$("#red").addClass("red-bg-active");
 	      	$("#green").addClass("green-bg-active");
@@ -346,7 +279,6 @@ view = {
 		$("#turnDisplay").text("WINNER").fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 	},
 
-
 	displayCount: function(){
 		var count = controller.count.get();
 		if (count>=0 && count<10){
@@ -363,35 +295,27 @@ view = {
 		}
 	},
 
-
 	displayColorSequence: function(){
-		
 		var x= setTimeout(function () {    
-	      	$(".color").removeClass("green-bg-active red-bg-active yellow-bg-active blue-bg-active");
-	      	                       
+	      	$(".color").removeClass("green-bg-active red-bg-active yellow-bg-active blue-bg-active");   	                       
 	   }, 1000);
 
 		var y =setTimeout(function () {    
-	      	var colorCount = controller.colorCount.get();
-	      	var colorSequence = controller.colorSequence.get();
+	      	var colorCount = controller.computer.colorCount.get();
+	      	var colorSequence = controller.computer.colorSequence.get();
 	      	if(colorCount<colorSequence.length){
 	      		var color = colorSequence[colorCount];
 	      		var audio = document.getElementById(color+"Audio");
 				audio.play();
 	      		$("#"+color).addClass(color+"-bg-active");
 	      	} else if (colorCount===colorSequence.length){
-	      		controller.turn.set("player");
-	      		// setTimeout(function () {    
-	      			view.displayTurn("player");
-	      			view.colorCursor.add();
-	      	                 
-	   			// }, 1000);
-	      		
+	      		controller.turn.set("player");   
+	      		view.displayTurn("player");
+	      		view.colorCursor.add();
 	      	}     
 	      	
-	      	controller.colorCount.addTo();
-	      	//colorCount must be established again because of the addTo above
-	      	colorCount = controller.colorCount.get();                     
+	      	controller.computer.colorCount.addTo();
+	      	colorCount = controller.computer.colorCount.get();                     
 	      	if (colorCount <= colorSequence.length) {          
 	         	view.displayColorSequence();           
 	      	}                       
@@ -400,13 +324,12 @@ view = {
 		$("#start").on("click",function(){
 			clearTimeout(x);
 			clearTimeout(y);
-		});
-			
+		});		
 	},
-
 };
 
-view.startButtonHandler();
-view.colorHandler();
-view.displayCount();
-view.strictButtonHandler();
+
+/*====================
+	INITIALIZATION
+====================*/
+controller.init();
